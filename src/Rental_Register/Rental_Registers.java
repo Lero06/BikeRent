66b61aea684f3d53c2ccd1036e324c46457c7f42
Lero <lesanchezro@est.utn.ac.cr>
2025-06-12 10:37:18 -0600

    Clase Rental_Register
    Se agrego la clase Rental_Register con sus atributos,metodos getters setters constructor y tostring

---
+++ src/Rental_Register/Rental_Registers.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rental_Register;
+
+/**
+ *
+ * @author llean
+ */
+public class Rental_Registers {
+    
+}
