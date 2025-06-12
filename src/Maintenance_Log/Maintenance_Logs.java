dba51fd7ba547c6b6c01a940addbfb6a2c8d0822
Lero <lesanchezro@est.utn.ac.cr>
2025-06-12 10:51:22 -0600

    Clase Maintenance_Log
    Se agrego la clase Maintenance_Log con sus respectivos atributos metodos getters setters constructor y toString

---
+++ src/Maintenance_Log/Maintenance_Logs.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Maintenance_Log;
+
+/**
+ *
+ * @author llean
+ */
+public class Maintenance_Logs {
+    
+}
